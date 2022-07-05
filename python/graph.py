5b1e361b3dfce939455b033a7d06db29e688e587
ggace <skydream5485@gmail.com>
2022-07-05 18:13:32 +0900

    graph.py update - 같은 w, b 배제

--- python/graph.py
+++ python/graph.py
@@ -27,18 +27,24 @@ xs = []
 ys = []
 
 f = open("../files/result.txt", "r")
-   
+
+current_w = 1
+current_b = 0
+
 for x in f.read().split("\n")[:-1]:
-    xs = []
-    ys = []
-    for i in range(min_value[0], max_value[0] + 1):
-        xs.append(i)
-        ys.append(float(x.split(": ")[0]) * i + float(x.split(": ")[1]))
-        
-    plt.plot(xs, ys)
+    if(float(x.split(": ")[0]) != current_w or current_b != float(x.split(": ")[1])):
+        xs = []
+        ys = []
+        for i in range(min_value[0], max_value[0] + 1):
+            xs.append(i)
+            ys.append(float(x.split(": ")[0]) * i + float(x.split(": ")[1]))
+            
+        plt.plot(xs, ys)
+        plt.scatter(xs_value, ys_value)  
+        plt.pause(0.000001)
+        current_w = float(x.split(": ")[0])
+        current_b = float(x.split(": ")[1])
     
-    plt.pause(0.001)
-    plt.scatter(xs_value, ys_value)  
     
     
     
